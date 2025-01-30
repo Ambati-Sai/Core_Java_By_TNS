8019e06b92b2cbad65f9b2d8a2831ccedb00a0a3
CSELAB6 <CSELAB6@DESKTOP-SJ271IP>
2025-01-30 10:09:54 +0530

    example of private access modifier

---
+++ CoreJava/src/com/tns/example/PrivateMain.java
@@ -0,0 +1,11 @@
+package com.tns.example;
+
+public class PrivateMain {
+	private int num = 10;
+	public static void main(String[] args) {
+		Demo d = new Demo();
+		System.out.println(d.num);
+	}
+
+
+}
