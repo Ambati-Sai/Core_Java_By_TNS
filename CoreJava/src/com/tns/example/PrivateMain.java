cfff586cff69187c9a17065e5f5c03a78a17c1f0
CSELAB6 <CSELAB6@DESKTOP-SJ271IP>
2025-01-30 10:15:47 +0530

    recorrect the code

--- CoreJava/src/com/tns/example/PrivateMain.java
+++ CoreJava/src/com/tns/example/PrivateMain.java
@@ -1,11 +1,9 @@
 package com.tns.example;
 
 public class PrivateMain {
-	private int num = 10;
+	private int a = 10;
 	public static void main(String[] args) {
-		Demo d = new Demo();
-		System.out.println(d.num);
+		PrivateMain d = new PrivateMain();
+		System.out.println(d.a);
 	}
-
-
 }
