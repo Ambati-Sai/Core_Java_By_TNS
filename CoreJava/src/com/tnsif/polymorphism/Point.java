70876e51b93ca750e76a3c9427f9f322a163ae81
CSELAB6 <CSELAB6@DESKTOP-SJ271IP>
2025-02-04 10:39:59 +0530

    examples for polymorphism

---
+++ CoreJava/src/com/tnsif/polymorphism/Point.java
@@ -0,0 +1,22 @@
+package com.tnsif.polymorphism;
+
+public class Point {
+	
+	private float a;
+	private float b;
+	
+	public Point() {
+		a=3.0f;
+		b=6.0f;
+		
+	}
+	public Point(float f) {
+		this.a=a;
+		b=1.0f;
+		
+	}
+	public Point(float a, float y) {
+		
+	}
+
+}
---
+++ CoreJava/src/com/tnsif/superkeyword/Parentdemo.java
@@ -0,0 +1,11 @@
+package com.tnsif.superkeyword;
+
+public class Parentdemo {
+	
+	int a=70;
+	
+	void drink() {
+		System.out.println("coffe");
+	}
+
+}
