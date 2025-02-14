390c2176dd7beb8d0861754971b0f973378b64df
CSELAB6 <CSELAB6@DESKTOP-SJ271IP.mshome.net>
2025-02-14 11:08:55 +0530

    String immutabal example

---
+++ CoreJava/src/com/tns/strings/Stringexample.java
@@ -0,0 +1,13 @@
+package com.tns.strings;
+
+public class Stringexample {
+	public static void main(String[] args) {
+		String s = "Sai";
+		s.concat("Ambati");
+		System.out.println("Name:"+s);
+		s = s.concat(" Ambati");
+		System.out.println("Full Name:"+s);
+		
+	}
+
+}
