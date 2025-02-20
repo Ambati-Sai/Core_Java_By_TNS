1fa76b89f7c0097764cf3e16553efb424e9cab94
CSELAB6 <CSELAB6@DESKTOP-SJ271IP.mshome.net>
2025-02-20 11:22:37 +0530

    lamda expressing using parameters

--- CoreJava/src/com/tns/lamdaexpressions/Interfacedemo.java
+++ CoreJava/src/com/tns/lamdaexpressions/Interfacedemo.java
@@ -1,6 +1,6 @@
 package com.tns.lamdaexpressions;
 
 public interface Interfacedemo {
-	void show();
+	void show(int a);
 
 }
--- CoreJava/src/com/tns/lamdaexpressions/Lamdamain.java
+++ CoreJava/src/com/tns/lamdaexpressions/Lamdamain.java
@@ -2,8 +2,8 @@ package com.tns.lamdaexpressions;
 
 public class Lamdamain {
 	public static void main(String[] args) {
-		Interfacedemo id = ()-> {System.out.println("lamda Expressions");};
-		id.show();
+		Interfacedemo id = (a)-> {System.out.println("Square of a number:"+(a*a));};
+		id.show(2);
 	}	
 
 }
